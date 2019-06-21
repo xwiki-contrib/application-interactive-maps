abdff9c0b1ac4bb828d810b3175719bc5c6765d4
GinPachi <m.fawaadali98@gmail.com>
2019-06-21 22:45:32 +0500

    INTMAP-13: Fullscreen mode + [Misc] Bug fixes, code optimization

--- application-interactive-maps-test/application-interactive-maps-test-tests/src/test/it/org/xwiki/intmap/test/ui/InteractiveMapsTest.java
+++ application-interactive-maps-test/application-interactive-maps-test-tests/src/test/it/org/xwiki/intmap/test/ui/InteractiveMapsTest.java
@@ -17,7 +17,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.xwiki.interactivemaps.test.ui;
+package org.xwiki.intmap.test.ui;
 
 import org.junit.Test;
 import org.xwiki.test.ui.AbstractTest;
